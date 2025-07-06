8f24725de6b991e4f8c0c87171b12c1de8f8ef90
mishdif <yoavishita@gmail.com>
2025-07-06 21:46:51 +0300

    Update .env (prod and dev)

--- pubs-tabs/src/firebase.js
+++ pubs-tabs/src/firebase.js
@@ -8,12 +8,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHgbyYVlW1ikzMAcnnIdKqhgwVnuYqxJ4",
-  authDomain: "pubs-tabs.firebaseapp.com",
-  projectId: "pubs-tabs",
-  storageBucket: "pubs-tabs.firebasestorage.app",
-  messagingSenderId: "883768752530",
-  appId: "1:883768752530:web:adb38690bad6d21debe94b"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
